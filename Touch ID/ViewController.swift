d9a4e86f441522ec86f2243e35117bbb47da523a
Obed D <obed@obeds-macbook-pro-184.local>
2018-11-23 15:09:07 +0700

    Initial Commit

---
+++ Touch ID/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Touch ID
+//
+//  Created by Obed RPL D on 23/11/18.
+//  Copyright © 2018 Syams.org. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
